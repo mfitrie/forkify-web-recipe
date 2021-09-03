5927aeea27056a6112c43ad228f9b5919671d304
mfitrie <mfitrie78@gmail.com>
2021-09-03 23:44:10 +0800

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -159,10 +159,6 @@ const controlAddRecipe = async function(newRecipe){
 
 };
 
-const newFeature = function(){
-  console.log('welcome to the application');
-}
-
 const init = function(){
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipe);
@@ -171,7 +167,6 @@ const init = function(){
   searchView.addHandlerSearch(controlSearhResults);
   paginationViews.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
